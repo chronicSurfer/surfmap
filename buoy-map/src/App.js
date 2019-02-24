9832b8dbc68c6aca29f0065364d33346981c8c3d
chronicSurfer <kyle.umbarger@gmail.com>
2019-02-24 15:37:01 -0800

    Google Maps on screen with centered location at hb pier

--- buoy-map/src/App.js
+++ buoy-map/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class App extends Component {
   render() {
     return (
-      console.log('this is filler text')
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Track your surf!
+          </p>
+        </header>
+        <Map 
+        google={this.props.google}
+        initialCenter={{
+          lat: 33.655716,
+          lng: -118.003551
+        }}
+        zoom={12}>
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            {/* <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div> */}
+        </InfoWindow>
+      </Map>
+      </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyDazIMQhUQPgXuo9qEBvl6n-tyUBvUxCAg')
+})(App)
